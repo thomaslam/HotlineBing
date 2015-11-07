228f52c4d7086d4581086fcc16b75b3060479c35
Cooper Pellaton <c@cepp.at>
2015-11-07 03:41:52 -0500

    Define returnable variables for the bing queries.

--- server/server.js
+++ server/server.js
@@ -69,26 +69,26 @@ app.post('/texts', function(req, res) {
 	// console.log("******************");
 	console.log(note);
 	console.log('Number is ' + req.body.From);
-	//queries Bing for the search term that the user responded to the initial message with
-	// Bing.web(note, {
-	//     top: 10,  // Number of results (max 50)
-	//     skip: 3,   // Skip first 3 results
-	//   }, function(error, res, body){
+	//Queries Bing for the search term that the user responded to the initial message with.
+	Bing.web(note, {
+		top: 10, // Number of results (max 50)
+		skip: 0, // Skip first 3 results
+	}, function(error, res, body) {
 
-	//   // body has more useful information, but for this example we are just
-	//   // printing the first two results
-	//   console.log(body.d.results[0]);
-	//   console.log(body.d.results[1]);
-	// });
+		// body has more useful information, but for this example we are just
+		// printing the first two results
+		var searchResponse1 = body.d.results[0];
+		var searchResponse2 = body.d.results[1];
+	});
 
 	//A list of all the possible messages that the user can be asked.
 	var userResponseList = {
 
 	};
 
-  if (note === "priceline" || "Priceline") {
-    var str = "What do you want to know?";
-  }
+	if (note === "priceline" || "Priceline") {
+		var str = "What do you want to know?";
+	}
 
 	var msgArray = {
 		"Thanks for texting Hotline-Bing! You can respond to this message with a general search term and get the results, or with PRICELINE and we will initiate a Priceline search. Text 'Thank You' at any point to stop searching."
@@ -97,18 +97,19 @@ app.post('/texts', function(req, res) {
 		"For what dates do you want the hotel?": 3,
 		"What is your maximum price per night?": 4,
 		"Here is a list of hotels: " + hotelListFromAPI: 5,
-			"Here is your hotel: " + hotelSelectorFromAPI: 6,
-			"This is the hotel you're staying in. Would you like to book it?": 7 "Your hotel has been booked": 8
+		"Here is your hotel: " + hotelSelectorFromAPI: 6,
+		"This is the hotel you're staying in. Would you like to book it?": 7,
+    "Your hotel has been booked": 8
 	};
 
 	//TODO: hotelListFromAPI-- returns a list of hotels that meet the criteria returned by the user in response to message prompts 2, 3 & 4.
 
-  //Defined a local escape value. No other condition makes this true and therefore it's false in all other instances.
+	//Defined a local escape value. No other condition makes this true and therefore it's false in all other instances.
 	if (note === "Thank You" || "Thank you" || "thank you" || "Thank you!" || "thank you!") {
 		var exit = true;
 	}
 
-  //Add a global escape with a boolean.
+	//Add a global escape with a boolean.
 	if (exit == true) {
 		//DO NOTHING
 	} else {
